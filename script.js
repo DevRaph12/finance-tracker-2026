90a971e3cc1f1c81a61fad2b718e321d48d73078
Raphael <179272591+DevRaph12@users.noreply.github.com>
2026-01-24 15:05:37 -0300

    Varibales needed to start working on my script updated

---
+++ script.js
@@ -0,0 +1,15 @@
+//Elements for the event listener
+
+const form = document.getElementById("finance-form");
+
+//Elements for the input fields
+
+const insputDescription = document.querySelector("#desc");
+const inputAmount = document.querySelector("#amount");
+const inputType = document.querySelector("#type");
+
+//Elements for displaying the data
+
+const displayIncomes = document.querySelector("#incomes-value");
+const displayExpenses = document.querySelector("#expenses-value");  
+const displayTotal = document.querySelector("#total-value");
